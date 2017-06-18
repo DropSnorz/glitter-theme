8a20393e5b5fd42275c0f40ee2dc4addf81e0bbe
Arthur POIRET <dropsnorz@gmail.com>
2017-06-18 07:34:04 +0000

    Hide long article text

--- php/home.php
+++ php/home.php
@@ -19,27 +19,39 @@
 		<a href="<?php echo $Post->permalink() ?>"><h3><?php echo $Post->title() ?></h3></a>
 
 		<hr>
+
+		<div class="ellipse"> </div>
+
 		
-		<div class="post-content">
+		<div class="post-content post-preview">
 		        <?php echo $Post->content(false) ?>
 
 		        <?php if($Post->readMore()) { ?>
 		        <a class="read-more" href="<?php echo $Post->permalink() ?>"><?php $Language->p('Read more') ?></a>
 			<?php } ?>
+
 		</div>
 
+
 		<div class="foot-post card-footer">
-			<div><?php echo $Post->date(); ?></div>
-			<?php
-				$links = array();
-				$tags = $Post->tags(true);
+			<div class="col-xs-8">
+				<div><?php echo $Post->date(); ?></div>
+				<?php
+					$links = array();
+					$tags = $Post->tags(true);
 
-				foreach($tags as $tagKey=>$tagName) {
-					$links[] = '<a class="post-tag" href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">#'.$tagName.'</a>';
-				}
+					foreach($tags as $tagKey=>$tagName) {
+						$links[] = '<a class="post-tag" href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">#'.$tagName.'</a>';
+					}
 
-				echo implode(', ', $links);
-			?>
+					echo implode(', ', $links);
+				?>
+			</div>
+			<div class="col-xs-4">
+				<div class="pull-right">
+					<a class="btn btn-sm btn-default" href="<?php echo $Post->permalink() ?>">Lire</a>
+				</div>
+			</div>
 		</div>
 
 		<!-- Plugins Post End -->
